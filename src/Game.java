a16704bade9fda0ead4bd7548a39579e3119686d
enes_kchmt <eneskocaahmet0@gmail.com>
2022-08-02 14:00:18 +0300

    Macera oyunu kodları geliştirilip yazıldı

--- src/Game.java
+++ src/Game.java
@@ -11,6 +11,7 @@ public class Game {
         Player player = new Player("Mustafa");
         System.out.println("Sayın " + player.getName() + " bu karanlık ve sisli odaya hoşgeldiniz !! Burada yaşanılan her şey gerçek ! ");
         System.out.println("Lütfen bir karakter seçiniz ! ");
+        System.out.println("***************************************************************");
         player.selectChar();
 
         Location location = null;
--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Player {
     private int damage;
     private int health;
+    private int orjinalHealth;
     private int money;
     private String charName;
     private String name;
@@ -44,30 +45,36 @@ public class Player {
                 initPlayer(new Samurai());
 
         }
-        System.out.println("Karakter : " + getCharName() +
+        /*System.out.println("Karakter : " + getCharName() +
                         ", Hasar : " + getDamage() +
                         ", Sağlık : " + getHealth() +
                         ", Para : " + getMoney());
+
+         */
 
 
     }
     public void initPlayer(GameChar gameChar) {
         this.setDamage(gameChar.getDamage());
         this.setHealth(gameChar.getHealth());
+        this.setOrjinalHealth(gameChar.getHealth());
         this.setMoney(gameChar.getMoney());
         this.setCharName(gameChar.getName());
     }
     public void printInfo() {
         System.out.println("Silah : " + this.getInventory().getWeapon().getName() +
-                ", Zırh :  " + this.getInventory().getWeapon().getName() +
+                ", Zırh : " + this.getInventory().getArmor().getName() +
                 ", Bloklama : " + this.getInventory().getArmor().getBlock() +
-                ", Hasar : " + this.getDamage() +
+                ", Hasar : " + this.getTotalDamage() +
                 ", Sağlık : " + this.getHealth() +
                 ", Para : " + this.getMoney());
     }
 
-    public int getDamage() {
+    public int getTotalDamage() {
         return damage + this.getInventory().getWeapon().getDamage();
+    }
+    public int getDamage() {
+        return damage;
     }
 
     public void setDamage(int damage) {
@@ -79,6 +86,9 @@ public class Player {
     }
 
     public void setHealth(int health) {
+        if (health < 0) {
+            health = 0;
+        }
         this.health = health;
     }
 
@@ -112,5 +122,21 @@ public class Player {
 
     public void setInventory(Inventory inventory) {
         this.inventory = inventory;
+    }
+
+    public int getOrjinalHealth() {
+        return orjinalHealth;
+    }
+
+    public void setOrjinalHealth(int orjinalHealth) {
+        this.orjinalHealth = orjinalHealth;
+    }
+
+    public Scanner getInput() {
+        return input;
+    }
+
+    public void setInput(Scanner input) {
+        this.input = input;
     }
 }
